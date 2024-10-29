02e81bd4ad549b4a5a8862bdf581891ac8e0e391
Lily Hirano <147770952+lilyhirano@users.noreply.github.com>
2024-10-29 11:14:36 -0700

    Add files via upload

--- smithWatermanCalc.py
+++ smithWatermanCalc.py
@@ -1,12 +1,15 @@
 import numpy as np
 
-dna1 = "A"   #top squence
-dna2 = "A"   #side sequence
+dna1 = "TGTTACGGAGTCTCTGCTCGC"   #top squence
+dna2 = "GGTTGACTACGCTCTGAGAGATC"   #side sequence
 
 gapPenalty = -2
-matchScore = 1
-mismatchScore = -1
+matchScore = 3
+mismatchScore = -3
 totalScore = 0
+matrixSW = np.matrix([0])
+rowLen = len(dna1)
+columnLen = len(dna2)
 
 def createMatrix(matrixSW, rowLen, columnLen):
 
@@ -34,7 +37,6 @@ def findLeftScore(column, row, matrixSW):
     cellScore = gapPenalty + matrixSW[row - 1, column ]
     return cellScore
 
-
 def calcCell(column, row, dna1, dna2, matrixSW):
     #if the same nucleotide
     if dna1[column - 1] == dna2[row - 1]:
@@ -54,7 +56,6 @@ def calcCell(column, row, dna1, dna2, matrixSW):
 
     return maxScore
 
-
 def calcRow(row, dna1, dna2, matrixSW, rowLen):
     rowIndex = 1
     while rowIndex <= rowLen:
@@ -65,6 +66,7 @@ def calcRow(row, dna1, dna2, matrixSW, rowLen):
         rowIndex += 1
 
     return matrixSW
+
 def calcMatrix(dna1, dna2, matrixSW, columnLen, rowLen):
     index = 1
     while index <= columnLen:
@@ -72,17 +74,64 @@ def calcMatrix(dna1, dna2, matrixSW, columnLen, rowLen):
         index += 1
     return matrixSW
 
+def calcSequence(matrixSW, dna1, dna2):
+    local1 = ""
+    local2 = ""
+    maxValue = np.max(matrixSW)
+    if maxValue > 0:
+        maxLocation = np.unravel_index(matrixSW.argmax(), matrixSW.shape)
+        
+        row = maxLocation[0] - 1 
+        column = maxLocation[1] - 1
+
+        while matrixSW[row , column] != 0: #while not at a 0
+
+            diagonal = matrixSW[row - 1, column - 1]
+            left = matrixSW[row, column - 1]
+            up = matrixSW[row - 1, column]
+
+            topScore = max(left, up, diagonal)
 
 
+            if topScore == diagonal: #if DNA matches or mismatches
+                local1 += dna1[column]
+                local2 += dna2[row]
+                row -= 1
+                column -= 1
+            elif topScore == up: #if highest score is above current cell
+                local1 += "-" #gap in dna1
+                local2 += dna2[row]
+                row -= 1
+            elif topScore == left: #if highest score is to the left of current cell
+                local2 += "-" #gap in dna2
+                local1 += dna1[column]
+                column -= 1
+    #run condition one more time after reaches 0 to evaluate last cell.
+    if dna1[column] == dna2[row]: 
+        local1 += dna1[column]
+        local2 += dna2[row]
+        row -= 1
+        column -= 1
+    elif topScore == up: 
+        local1 += "-"
+        local2 += dna2[row]
+        row -= 1
+    elif topScore == left: 
+        local2 += "-" 
+        local1 += dna1[column]
+        column -= 1
+
+    return (local1[::-1], local2[::-1])
+        
 
 
-matrixSW = np.matrix([0])
-rowLen = len(dna1)
-columnLen = len(dna2)
 
 matrixSW = createMatrix(matrixSW, rowLen, columnLen)
-
 matrixSW = calcMatrix(dna1, dna2, matrixSW, columnLen, rowLen)
 
+print(matrixSW)
 
+results = calcSequence(matrixSW, dna1, dna2)
 
+print(results[0])
+print(results[1])
